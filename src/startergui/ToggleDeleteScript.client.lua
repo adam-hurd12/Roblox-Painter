4ff87837c2bd59b0a68d832d2ad431547a437e46
Adam Hurd <adamhurd@btinternet.com>
2025-12-08 22:33:37 +0000

    Removed all deprecated files.

--- src/startergui/ToggleDeleteScript.client.lua
+++
@@ -1,8 +0,0 @@
-local ToggleEvent = game:GetService('ReplicatedStorage').Events:WaitForChild('ToggleDelete')
-
-script.Parent.InventoryGui.Main.ToggleDelete.MouseButton1Down:Connect(function()
-	
-	local plr = game.Players.LocalPlayer
-	
-	ToggleEvent:FireServer(plr, plr.leaderstats.delete.Value)
-end)
